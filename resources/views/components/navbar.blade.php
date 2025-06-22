5ec91b95768d532e78815b10fdfed22c85aa32b0
anggathestarboy <anggarathestarboy@gmail.com>
2025-06-22 16:01:14 +0700

    menambahkan model Post

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,7 +9,7 @@
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
               <a href="/" class="{{ request() -> is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}}  rounded-md  px-3 py-2 text-sm font-medium " aria-current="page">Home</a>
-                  <a href="/blog" class=" {{ request() -> is('blog') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md  px-3 py-2 text-sm font-medium ">Blog</a>         
+                  <a href="/posts" class=" {{ request() -> is('blog') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md  px-3 py-2 text-sm font-medium ">Blog</a>         
               <a href="/about" class="{{ request() -> is('about') ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3 py-2 text-sm font-medium">About</a>
               <a href="/contact" class="{{ request() -> is('contact') ? "bg-gray-900 text-white" : 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium">Contact</a>
  
@@ -79,7 +79,7 @@
       <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
         <a href="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
-        <a href="/blog" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Blog</a>
+        <a href="/posts" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Blog</a>
         <a href="/about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
         <a href="/contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
       </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
+use App\Models\Post;
+
+
+
+
 
 Route::get('/', function () {
     return view("home", ['title' => 'Home Page']);
@@ -8,9 +14,18 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view("about", ['title' => 'About'], ['name' => 'Anggara']);
 });
-Route::get('/blog', function () {
-    return view("blog", ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view("posts", ['title' => 'Blog', 'posts' => Post::all()]);
 });
+
+Route::get('/posts/{post:slug}', function(Post $post) {
+      
+
+
+        return view('post', ['title' => 'Single Post', 'post' => $post] );
+});
+
+
 Route::get('/contact', function () {
     return view("contact", ['title' => 'Contact']);
 });
